ceaef7ff3790aad9ae164380da727ed29f34b37a
rqphy <raphael.ferreira@hetic.net>
2023-06-04 22:50:29 +0200

    :sparkles: add build route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.scss"
 import Header from "./components/header"
+import Build from "./routes/build/build"
 import Home from "./routes/home/home"
 import { Routes, Route } from "react-router-dom"
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <Routes>
-                <Route index element={<Home />} />
-            </Routes>
-        </div>
-    )
+	return (
+		<div className="App">
+			<Header />
+			<Routes>
+				<Route index element={<Home />} />
+				<Route path="/build" element={<Build />} />
+			</Routes>
+		</div>
+	)
 }
 
 export default App
