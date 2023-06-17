86d745a1429b9dcd7f7f2f8a554724b1f43b2d8c
rqphy <raphael.ferreira@hetic.net>
2023-06-17 18:33:21 +0200

    :construction: make cube stacks on column

--- src/components/column/column.jsx
+++ src/components/column/column.jsx
@@ -2,7 +2,8 @@ import Cube from "../cube/cube"
 export default function Column() {
 	return (
 		<group>
-			<Cube />
+			<Cube pos={[0, 0, 0]} />
+			<Cube pos={[0, 1, 0]} color="blue" />
 		</group>
 	)
 }
