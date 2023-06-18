76e8ad1334303b7238f4e2dbbb67a925f37dff28
rqphy <raphael.ferreira@hetic.net>
2023-06-18 12:39:11 +0200

    :construction: builder > update build function to match new gameplay

--- src/components/column/column.jsx
+++ src/components/column/column.jsx
@@ -1,20 +1,8 @@
-import { useState } from "react"
 import Cube from "../cube/cube"
 export default function Column() {
-	const [cubeCount, setCubeCount] = useState(1)
-
-	const handleColumnClick = () => {
-		// update cubeCount
-		console.log(cubeCount + 1)
-		setCubeCount(cubeCount + 1)
-		// todo create cube
-	}
-
 	return (
-		<group onClick={handleColumnClick}>
-			{[...new Array(cubeCount)].map((el, index) => (
-				<Cube pos={[0, index, 0]} key={index} />
-			))}
+		<group>
+			<Cube pos={[0, 0, 0]} />
 		</group>
 	)
 }
--- src/components/cube/cube.jsx
+++ src/components/cube/cube.jsx
@@ -1,6 +1,20 @@
 export default function Cube({ pos = [0, 0, 0], color = "red" }) {
+	const handleCubeClick = (_event) => {
+		console.log(_event.point)
+		findBuildCoords(_event.point)
+	}
+
+	const findBuildCoords = (point) => {
+		const coords = [...Object.values(point)]
+		const directionIndex = coords.findIndex((x) => x % 0.5 === 0)
+		const newCoords = [0, 0, 0]
+		if (directionIndex === -1) return
+		newCoords[directionIndex] = coords[directionIndex] + 1
+		console.log(newCoords)
+	}
+
 	return (
-		<mesh position={pos}>
+		<mesh position={pos} onClick={handleCubeClick}>
 			<boxGeometry />
 			<meshBasicMaterial color={color} />
 		</mesh>
