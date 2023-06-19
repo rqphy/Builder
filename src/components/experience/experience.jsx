09e3c310574aae40347b685e36a78f4ca2e14053
rqphy <raphael.ferreira@hetic.net>
2023-06-19 18:49:02 +0200

    :art: reset camera position

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -9,8 +9,7 @@ export default function Experience() {
 				fov: 45,
 				near: 0.1,
 				far: 200,
-				// position: [2, 8, 8],
-				position: [0, 2, 8],
+				position: [2, 8, 8],
 			}}
 		>
 			<directionalLight
