3d53565c4cd8200448011350e8b22171800a4942
rqphy <raphael.ferreira@hetic.net>
2023-06-19 18:44:26 +0200

    :sparkles: builder > update build algo

--- src/components/column/column.jsx
+++ src/components/column/column.jsx
@@ -1,8 +1,37 @@
+import { useState } from "react"
 import Cube from "../cube/cube"
-export default function Column() {
+export default function Column({ size = 2 }) {
+	const [cubesPositions, setCubesPositions] = useState([[0, 0, 0]])
+
+	const handleCubeClick = (_event) => {
+		const buildCoords = findBuildCoords(
+			_event.point,
+			_event.eventObject.position
+		)
+		console.log(buildCoords)
+		setCubesPositions([...cubesPositions, buildCoords])
+	}
+
+	const findBuildCoords = (point, eventObjectPos) => {
+		const objectPos = [...Object.values(eventObjectPos)]
+		const pointerPos = [...Object.values(point)]
+		const directionIndex = pointerPos.findIndex((x) => x % 1 === 0)
+		const newCoords = [...objectPos]
+		if (directionIndex === -1) return
+		newCoords[directionIndex] = pointerPos[directionIndex] + size / 2
+		return newCoords
+	}
+
 	return (
 		<group>
-			<Cube pos={[0, 0, 0]} />
+			{cubesPositions.map((position) => (
+				<Cube
+					key={position.toString()}
+					size={size}
+					pos={position}
+					handleClick={handleCubeClick}
+				/>
+			))}
 		</group>
 	)
 }
--- src/components/cube/cube.jsx
+++ src/components/cube/cube.jsx
@@ -1,21 +1,11 @@
-export default function Cube({ pos = [0, 0, 0], color = "red", size = 2 }) {
-	const handleCubeClick = (_event) => {
-		console.log(_event.eventObject.position)
-		findBuildCoords(_event.point, _event.eventObject.position)
-	}
-
-	const findBuildCoords = (point, eventObjectPos) => {
-		const objectPos = [...Object.values(eventObjectPos)]
-		const pointerPos = [...Object.values(point)]
-		const directionIndex = pointerPos.findIndex((x) => x % 1 === 0)
-		const newCoords = [...objectPos]
-		if (directionIndex === -1) return
-		newCoords[directionIndex] = pointerPos[directionIndex] + size / 2
-		console.log(newCoords)
-	}
-
+export default function Cube({
+	pos = [0, 0, 0],
+	color = "red",
+	size = 2,
+	handleClick,
+}) {
 	return (
-		<mesh scale={size} position={pos} onClick={handleCubeClick}>
+		<mesh scale={size} position={pos} onClick={handleClick}>
 			<boxGeometry />
 			<meshBasicMaterial color={color} />
 		</mesh>
