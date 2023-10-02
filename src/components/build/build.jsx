d7f08d3077feb60b7b43d829409dbe34270039ae
rqphy <raphael.ferreira@hetic.net>
2023-10-02 17:28:51 +0100

    :beer: no idea what I did with that but it still works

--- src/components/build/build.jsx
+++ src/components/build/build.jsx
@@ -1,73 +1,74 @@
 import { useState } from "react"
 import Cube from "../cube/cube"
 export default function Build({ size = 2 }) {
-    const [cubesList, setCubesList] = useState([
-        {
-            pos: [0, 0, 0],
-            color: "red",
-        },
-    ])
+	const [cubesList, setCubesList] = useState([
+		{
+			pos: [0, 0, 0],
+			color: "red",
+		},
+	])
 
-    const handleCubeClick = (_event) => {
-        // obstruct raycaster
-        _event.stopPropagation()
+	const handleCubeClick = (_event) => {
+		// obstruct raycaster
+		_event.stopPropagation()
 
-        // get new cube coords
-        const buildCoords = findBuildCoords(
-            _event.point,
-            _event.eventObject.position
-        )
+		// get new cube coords
+		const buildCoords = findBuildCoords(
+			_event.point,
+			_event.eventObject.position
+		)
 
-        // build new cube
-        if (!buildCoords) return
-        setCubesList([
-            ...cubesList,
-            {
-                pos: buildCoords,
-                color: `hsl(${Math.random() * 360}, 100%, 75%)`,
-            },
-        ])
-    }
+		// build new cube
+		if (!buildCoords) return
+		setCubesList([
+			...cubesList,
+			{
+				pos: buildCoords,
+				color: `hsl(${Math.random() * 360}, 100%, 75%)`,
+			},
+		])
+	}
 
-    // todo: fix undefined coords issue
-    const findBuildCoords = (point, eventObjectPos) => {
-        const objectPos = [...Object.values(eventObjectPos)]
-        const pointerPos = [...Object.values(point)]
-        const directionIndex = pointerPos.findIndex((x) => x % 1 === 0)
+	// todo: fix undefined coords issue
+	const findBuildCoords = (point, eventObjectPos) => {
+		const objectPos = [...Object.values(eventObjectPos)]
+		const pointerPos = [...Object.values(point)]
+		const directionIndex = pointerPos.findIndex((x) => x % 1 === 0)
 
-        // Check limits
-        if (
-            (objectPos[directionIndex] > size ||
-                objectPos[directionIndex] < -size) &&
-            directionIndex != 1
-        )
-            return false
+		console.log(objectPos)
 
-        const newCoords = [...objectPos]
-        if (directionIndex === -1) return
+		// Check limits
+		if (
+			(objectPos[directionIndex] > size ||
+				objectPos[directionIndex] < -size) &&
+			directionIndex != 1
+		)
+			return false
 
-        // check direction
-        if (pointerPos[directionIndex] - objectPos[directionIndex] > 0) {
-            newCoords[directionIndex] = pointerPos[directionIndex] + size / 2
-        } else {
-            newCoords[directionIndex] = pointerPos[directionIndex] - size / 2
-        }
-        console.log(newCoords, pointerPos)
+		const newCoords = [...objectPos]
+		if (directionIndex === -1) return
 
-        return newCoords
-    }
+		// check direction
+		if (pointerPos[directionIndex] - objectPos[directionIndex] > 0) {
+			newCoords[directionIndex] = pointerPos[directionIndex] + size / 2
+		} else {
+			newCoords[directionIndex] = pointerPos[directionIndex] - size / 2
+		}
 
-    return (
-        <group>
-            {cubesList.map((cube) => (
-                <Cube
-                    key={cube.pos.toString()}
-                    size={size}
-                    pos={cube.pos}
-                    color={cube.color}
-                    handleClick={handleCubeClick}
-                />
-            ))}
-        </group>
-    )
+		return newCoords
+	}
+
+	return (
+		<group>
+			{cubesList.map((cube) => (
+				<Cube
+					key={cube.pos.toString()}
+					size={size}
+					pos={cube.pos}
+					color={cube.color}
+					handleClick={handleCubeClick}
+				/>
+			))}
+		</group>
+	)
 }
